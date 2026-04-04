a18437972ce8937a5ae694a9d6a91e167c55baf2
biniyam tegegne <biniyamtegegne21gmail.com>
2026-04-04 06:02:23 +0300

    footer first has commited!

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -1,18 +1,57 @@
 import React from 'react'
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import "./Footer.css"
 
 function Footer() {
   return (
   
   <div className='footer_outer_container'>
     <div className='footer_iner_container'>
-     <div className='footer_icon'>
-      <FacebookIcon/>
-      <InstagramIcon/>
+     <div className='footer_icons'>
+       <FacebookIcon/>
+       <InstagramIcon/>
+       <YouTubeIcon/>
+
       </div>
-       </div>
+      <div className='footer-data'>
+        <div>
+          <ul>
+             <li>Audio description</li>
+             <li>Investor Relations</li>
+             <li>Legal Notices</li>
+          </ul>
         </div>
+        <div>
+          <ul>
+             <li>Help Center</li>
+             <li>Jobs</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Gift Cards</li>
+             <li>Terms of Use</li>
+              <li>Corporate Information</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Media Center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+     </div>
+     <div className='service_code'>
+        
+     </div>
+       
+       </div>
+        </div>  
      )
 }
 
